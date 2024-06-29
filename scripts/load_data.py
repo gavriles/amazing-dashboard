1bea1d4d320a498dc0d4c0374ea265dc86e2d25e
Bo <ahsaira@tutamail.com>
2024-06-29 19:50:32 -0300

    Update load_data.py

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -3,8 +3,8 @@ import pandas as pd
 def load_data():
     df1 = pd.read_csv('../data/2024-06-28 (2mes) Web3 Founders.csv')
     df2 = pd.read_csv('../data/2024-06-28 (2mes) HUMANS_DAO.csv')
-    df3 = pd.read_csv('../data/2024-06-28 (2mes) KRA_WEB3_netvorking.csv')
-    df4 = pd.read_csv('../data/2024-06-28 (2mes) startech_networking.csv')
+    df3 = pd.read_csv('../data/2024-06-28 (2mes) KRA_WEB_3.csv.csv')
+    df4 = pd.read_csv('../data/2024-06-28 (2mes) .csv')
     df5 = pd.read_csv('../data/2024-06-28 (2mes) startech_networking.csv')
     df6 = pd.read_csv('../data/2024-06-28 (2mes) startech_networking.csv')
     return df1, df2, df3
